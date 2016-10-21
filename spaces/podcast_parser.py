1d9c000ced449348ef94576a8c912e9021359daa
jypllx <jypllx@gmail.com>
2016-10-21 12:19:25 +0200

    storing more infos

--- spaces/podcast_parser.py
+++ spaces/podcast_parser.py
@@ -3,29 +3,39 @@
 
 import feedparser
 import psycopg2
+import datetime
 
 def parse_podcast(url):
     """Parses an XML feed and stores it to the db"""
 
     feed = feedparser.parse(url)
-    print("======")
-    # for key, value in feed.iteritems() :
-    # 	print("%s:   %s" % (key, value))
+    # print("======")
+    # for key, value in feed.entries[0].iteritems() :
+    #     print("%s:   %s" % (key, value))
     # print("======")
 
-    print(feed['feed']['tags'][0]['term'])
+    # print(feed.feed.tags[0].term)
 
     conn = psycopg2.connect("dbname=spaces user=spaces")
 
-    feedId = getFeedId(conn, url, feed)
+    channelId = getChannelId(conn, url, feed)
+
+    for item in feed.entries:
+        # print(item.title)
+        # print(item.id)
+        # print(item.duration)
+        if existsItem(conn, channelId, item):
+            pass
+        else:
+            saveItem(conn, channelId, item)
 
     conn.close()
 
-def getFeedId(conn, url, feed):
+def getChannelId(conn, url, feed):
     cur = conn.cursor()
 
-    query = "SELECT id FROM feeds WHERE name = '%s' AND url = '%s';" % (
-      feed['feed']['title'], 
+    query = "SELECT id FROM channels WHERE name = '%s' AND url = '%s';" % (
+      feed.feed.title, 
       url)
 
     cur.execute(query)
@@ -33,30 +43,50 @@ def getFeedId(conn, url, feed):
 
     if (len(res) > 1):
         print('More than one')
-        #raise Exception('Found multiple instance of (%s, %s)' % (feed['feed']['title'], url))
+        #raise Exception('Found multiple instance of (%s, %s)' % (feed.feed['title'], url))
     elif (len(res) == 0):
        print('None')
-       sql = "INSERT INTO feeds (url, name, genre, language, link) VALUES (%s, %s, %s, %s, %s);"
-
-       print(url)
-       print(feed['feed']['title'])
-       print(feed['feed']['tags'][0]['term'])
-       print(feed['feed']['language'])
-       print(feed['feed']['link'])
+       sql = "INSERT INTO channels (url, name, genre, language, link) VALUES (%s, %s, %s, %s, %s);"
+       print(feed.feed.link)
        cur.execute(sql, (url, 
-           feed['feed']['title'], 
-           feed['feed']['tags'][0]['term'], 
-           feed['feed']['language'], 
-           feed['feed']['link']))
+           feed.feed.title, 
+           feed.feed.tags[0].term, 
+           feed.feed.language, 
+           feed.feed.link))
        conn.commit()
        cur.execute(query)
        res = cur.fetchall()
+       conn.commit()
 
-    feedId = res[0]
-    print("feed id : %s" % (feedId))
+    channelId = res[0]
+    print("Channel id : %s" % (channelId))
 
     cur.close()
-    return feedId
+    return channelId
+
+def existsItem(conn, channelId, item):
+    cur = conn.cursor()
+
+    query = "SELECT id FROM items WHERE channel_id=%s AND name=%s"
+    cur.execute(query, (channelId, item.title))
+    res = cur.fetchall()
+    conn.commit()
+    cur.close
+    if (len(res) > 1):
+        print('More than one podcast for %s, %s' % (channelId, item.title))
+        raise Exception('Merde!!')
+    elif (len(res) == 1):
+        return True
+    else:
+        return False
+
+def saveItem(conn, channelId, item):
+    cur = conn.cursor()
+    sql="INSERT INTO items (channel_id, item_id, name, duration) VALUES (%s, %s, %s, %s);"
+    cur.execute(sql, (channelId, item.title, item.id, item.itunes_duration))
+    conn.commit()
+    cur.close()
+
 
 if __name__ == "__main__":
     parse_podcast('http://feeds.serialpodcast.org/serialpodcast')
